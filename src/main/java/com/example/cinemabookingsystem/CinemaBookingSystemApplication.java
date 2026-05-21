25dc66a30a8fd809a44a6fc0e07e33f09c576f53
Kassem Yahia <kassemhajyahia@gmail.com>
2026-05-21 20:57:54 +0300

    frontend

--- src/main/java/com/example/cinemabookingsystem/CinemaBookingSystemApplication.java
+++ src/main/java/com/example/cinemabookingsystem/CinemaBookingSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CinemaBookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaBookingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBookingSystemApplication.class, args);
+    }
 
 }
